2201007a2db04b6237698a28358b6e9bcb77fbda
Joseph Duffy <JosephDuffy@me.com>
2016-05-23 19:40:18 +0100

    Added more common types to ItemProviderTypeIndentifier

--- Action Extension/ItemProviderTypeIndentifier.swift
+++ Action Extension/ItemProviderTypeIndentifier.swift
@@ -12,9 +12,14 @@ import MobileCoreServices
 enum ItemProviderTypeIndentifier: RawRepresentable {
     case Item
     case Content
+    case URL
+    case PlainText
     case Image
     case JPEG
     case JPEG2000
+    case PNG
+    case MPEG4
+    case ContactCard
     case Unknown(raw: String)
 
     var rawValue: String {
@@ -23,12 +28,22 @@ enum ItemProviderTypeIndentifier: RawRepresentable {
             return kUTTypeItem as String
         case .Content:
             return kUTTypeContent as String
+        case .URL:
+            return kUTTypeURL as String
+        case .PlainText:
+            return kUTTypePlainText as String
         case .Image:
             return kUTTypeImage as String
         case .JPEG:
             return kUTTypeJPEG as String
         case .JPEG2000:
             return kUTTypeJPEG2000 as String
+        case .PNG:
+            return kUTTypePNG as String
+        case .MPEG4:
+            return kUTTypeMPEG4 as String
+        case .ContactCard:
+            return kUTTypeVCard as String
         case .Unknown(let rawValue):
             return rawValue
         }
@@ -40,12 +55,22 @@ enum ItemProviderTypeIndentifier: RawRepresentable {
             self = Item
         case (kUTTypeContent as String as String):
             self = Content
+        case (kUTTypeURL as String as String):
+            self = URL
+        case (kUTTypePlainText as String as String):
+            self = PlainText
         case (kUTTypeImage as String as String):
             self = Image
         case (kUTTypeJPEG as String as String):
             self = JPEG
         case (kUTTypeJPEG2000 as String as String):
             self = JPEG2000
+        case (kUTTypePNG as String as String):
+            self = PNG
+        case (kUTTypeMPEG4 as String as String):
+            self = MPEG4
+        case (kUTTypeVCard as String as String):
+            self = ContactCard
         // TODO: Add more conversions
         default:
             self = Unknown(raw: rawValue)
@@ -58,12 +83,22 @@ enum ItemProviderTypeIndentifier: RawRepresentable {
             return "Item"
         case .Content:
             return "Content"
+        case .URL:
+            return "URL"
+        case .PlainText:
+            return "Plain Text"
         case .Image:
             return "Image"
         case .JPEG:
-            return "JPEG"
+            return "Image (JPEG)"
         case .JPEG2000:
-            return "JPEG 2000"
+            return "Image (JPEG 2000)"
+        case .PNG:
+            return "Image (PNG)"
+        case .MPEG4:
+            return "Video (MPEG4)"
+        case .ContactCard:
+            return "Contact Card"
         case .Unknown(raw: _):
             return "Unknown"
         }
